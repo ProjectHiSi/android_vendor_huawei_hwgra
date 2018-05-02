2af4b9e0c3c7bc2af12a52a3c84a271b6a36afce
T1mpp4 <timi.rautamaki@gmail.com>
2018-05-02 22:30:26 +0300

    Update listall

--- listall.sh
+++ listall.sh
@@ -1,4 +1,17 @@
+# Creted by T1mpp4
+#
+# Usage:
+#
+#	in directory with .so files (lib, lib64, etc.):
+#		./listall.sh
+#		--> File 'function.txt'
+
+
+
 #!/bin/bash
+
+rm ./function.txt
+
 for filename in ./*.so; do
         echo $'\n'$'\n'$'\n'$'\n'$'\n'#####FILENAME$'\n' $filename$'\n'#####FILENAME$'\n'$'\n'$'\n'$'\n'$'\n' >> functions.txt
         nm -D $filename >> functions.txt
